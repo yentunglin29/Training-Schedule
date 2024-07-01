0bab190528ad38c4fed9b5c6fe920775ff2f9e07
tunglin29 <yentunglin29@gmail.com>
2024-07-01 16:31:36 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
 const multer = require('multer');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
