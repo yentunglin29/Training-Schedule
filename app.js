78d003d2d08159e54172d26c4d1427bc7a3f7a1a
yentunglin29 <yentunglin29@gmail.com>
2024-07-01 12:58:09 +0800

    Update for Heroku deployment

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const multer = require('multer');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Set up Multer to store files
 const storage = multer.diskStorage({
